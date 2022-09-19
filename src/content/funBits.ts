537d60c0c503b22400f666e4d721e783c55ed7e4
fredericoo <rincofrederico@gmail.com>
2022-09-19 18:54:39 +0100

    feat: add e2e tests

---
+++ src/content/funBits.ts
@@ -0,0 +1,12 @@
+const funBits = [
+	'I’m severly shortsighted: my glasses are super thick and I can’t see anything without them.',
+	'One of my cats, Tapi, fell (jumped, the little devil) from the 9th floor of the building I used to live. She didn’t break a single bone, but was hospitalised for two days!',
+	'I’ve lived in Sardegna, the big island in Italy, for three months. It was during winter at the peak of covid, so not a lot of beaches were enjoyed.',
+	'I’ve picked up most of my English accent from a friend from a town close to Birmingham, and still have a tinge of northern accent. Thanks Louis.',
+	'For uni, I went for Graphic Design, and only during my final project I realised I quite liked programming too. I’m still a designer at heart.',
+	"I've never had any broken bones!",
+	"To this day I haven't seen snow in my life. Muddy snow from London doesn't count.",
+	"My mum always tried to feed me sea food but I didn't like it. During several years she’d feed me it and pretend it’s chicken — I always found out.",
+];
+
+export default funBits;
